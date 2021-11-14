21f2c17092edabb700b97ae03efe93474a9daee7
Martin <caceresmartin22@gmail.com>
2021-11-14 15:40:14 -0300

    Added unit Testing

---
+++ System-tests/Voto_test.js
@@ -0,0 +1,6 @@
+Feature('Voto');
+
+Scenario('test something', ({ I }) => {
+	I.amOnPage('https://localhost:5001');
+	I.see('Cats');
+});
---
+++ System-tests/codecept.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost:5001/',
+      show: true,
+      browser: 'firefox'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'System-tests',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
